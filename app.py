e4629c94de5cab41cefc2e4c6697e9e3c26fae8e
Taunk, Digvijay <Digvijay.Taunk@akersolutions.com>
2022-10-12 20:04:12 +0530

    functional app. Converted to EXE

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-import wx 
-  
-#import the newly created GUI file 
-import form  
+import os
+import wx
+from PIL import Image
+
+import form
+
+
 class CalcFrame(form.MyFrame1): 
-    def __init__(self,parent): 
-        form.MyFrame1.__init__(self,parent)  
-		
-    def OnCompressClick1(self,event): 
+    def __init__(self, parent):
+        form.MyFrame1.__init__(self, parent)
+
+    def OnCompressClick1(self, event):
         num = int(self.txtSource.GetValue()) 
         self.txtDestination.SetValue (str(num*num)) 
 
-    def OnCompressClick(self,event): 
+    def OnCompressClick(self, event):
         source_path = self.txtSource.GetValue()
         dest_path = self.txtDestination.GetValue()
         quality = self.txtQuality.GetValue()
@@ -21,13 +24,15 @@ class CalcFrame(form.MyFrame1):
             fd = os.path.join(dest_path, filename)
             img = Image.open(fs)
             x, y = img.size
-            target_x = 1024
+            target_x = int(target_size)
             scale = target_x/x
             img_compressed = img.resize((int(x*scale), int(y*scale)), Image.Resampling.LANCZOS)
-            img_compressed.save(fd, quality=90, optimize=True)
+            img_compressed.save(fd, quality=int(quality), optimize=True)
+
 
 app = wx.App(False) 
 frame = CalcFrame(None) 
-frame.Show(True) 
-#start the applications 
-app.MainLoop() 
+frame.Show(True)
+
+# start the applications
+app.MainLoop()
